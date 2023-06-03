08d99b54e4b560a563996418aeaee2e62544e56a
MOGI601 <as.sd.show@gmail.com>
2023-06-03 21:32:44 +0900

    pythonNet 및 gesticulator 코드 삽입 (아직 gesticulator와의 pythonnet 연동은 하지 않음)

---
+++ MDD_Final/Assets/Scripts/ReadPythonPath/ReadPythonPath.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public static class ReadPythonPath
+{
+    private static string[] personalPaths = new string[5];
+
+    public static void SetFilePath()
+    {
+        int i = 0;
+        
+        StreamReader reader = new StreamReader(Application.dataPath + "/pathinfo.txt");
+
+        while (!reader.EndOfStream)
+        {
+            string[] _tempStr = reader.ReadLine().Split('>');
+            personalPaths[i] = _tempStr[1];
+            i++;
+        }
+        reader.Close();
+    }
+
+    public static string GetFilePath(int _index) // 0 : PYDLL 1:HOME 2:PACKAGE1 3:PACKAGE2 4:PACKAGE3
+    {
+        return personalPaths[_index];
+    }
+}
